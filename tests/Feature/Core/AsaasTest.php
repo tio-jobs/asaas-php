3c51ac3173ba0380e90c55dfdf56a94ba3e5c240
Dan Ang <daniel@sysanalyst.eu>
2024-01-15 23:41:20 +0100

    Use helper function in test

--- tests/Feature/Core/AsaasTest.php
+++ tests/Feature/Core/AsaasTest.php
@@ -7,7 +7,7 @@ it('api key is required when allowing sub-accounts', function(){
 
     Config::set('asaas-php.allow_sub_accounts', true);
 
-    AsaasPhp::withKey('')->customer()->get(id: '123');
+    asaasPhp()->customer()->get(id: '123');
 
 })->throws(ExceptionRequiredApiKey::class, 'You must provide an API key. You can do so by passing it as a parameter to the constructor or methods (e.g., make(apiKey) or withKey(apiKey)).');
 
